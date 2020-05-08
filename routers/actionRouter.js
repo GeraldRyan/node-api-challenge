4ced757ac7c39aab44261b50c15a69099f654e6d
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-08 11:03:26 -0500

    getting first to get requests for actions working

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -11,4 +11,44 @@ router.get('/', (req, res) =>{
   })
 })
 
+router.get('/:id', (req, res) =>{
+  actions.get(req.params.id)
+  .then(theAction =>{
+    res.status(200).json(theAction)
+  })
+  .catch(err =>{
+    res.status(500).json({message:"Action Not found"})
+  })
+})
+
+router.post('/', (req, res) =>{
+  actions.insert(req.body)
+  .then(allActions =>{
+    res.status(200).json(allActions)
+  })
+  .catch(err =>{
+    res.status(500).json({message:"Broken"})
+  })
+})
+
+router.put('/:id', (req, res) =>{
+  actions.update(req.params.id,req.body)
+  .then(allActions =>{
+    res.status(200).json(allActions)
+  })
+  .catch(err =>{
+    res.status(500).json({message:"Broken"})
+  })
+})
+
+router.delete('/:id', (req, res) =>{
+  actions.remove(req.params.id)
+  .then(allActions =>{
+    res.status(200).json(allActions)
+  })
+  .catch(err =>{
+    res.status(500).json({message:"Broken"})
+  })
+})
+
 module.exports = router

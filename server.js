f19085addba89d9f922106ae1fcd9602496e7b35
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-08 10:29:16 -0500

    server is fully running

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const actionRouter = require("./routers/actionRouter")
 const projectRouter = require("./routers/projectRouter")
 
 server.get('/', (req, res)=>{
-  es.status(200).json({message:"Server is running"})
+  res.status(200).json({message:"Server is running"})
 })
 
 module.exports = server
